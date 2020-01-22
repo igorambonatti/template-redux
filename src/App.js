fdedb90e9e5f9576e1b0866a0dd81f31861e7ab9
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-22 18:41:30 -0300

    up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import GlobalStyle from './styles/global';
+
+import history from './services/history';
+
+import store from './store/index';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
